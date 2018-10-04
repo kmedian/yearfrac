1a258113d217c61524e23620b7c304f3699f6149
Ivan Nešić <fonposta@gmail.com>
2018-10-04 12:27:20 +0200

    add vectorized behaviour to yearfrac function
    The function will return list as a result if any of the inputs are iterable. Also added some missing requirements.

--- test/test_yearfrac.py
+++ test/test_yearfrac.py
@@ -1,6 +1,8 @@
 import unittest
 from yearfrac import yearfrac
 from datetime import datetime
+import numpy as np
+import pandas as pd
 
 
 class Test_Yearfrac(unittest.TestCase):
@@ -16,6 +18,55 @@ class Test_Yearfrac(unittest.TestCase):
         d2 = datetime(2019, 3, 1)
         x = yearfrac(d1, d2, 'act_isda')
         self.assertEquals(round(x, 8), 0.20547945)
+
+    def test_vector_vector(self):
+        v1 = [datetime(2018, 12, 15), datetime(2018, 12, 15)]
+        v2 = [datetime(2019, 3, 1), datetime(2019, 3, 1)]
+        x = yearfrac(v1, v2, 'act')
+        self.assertEquals(
+            [round(x[0], 8), round(x[1], 8)],
+            [0.20547945, 0.20547945]
+        )
+
+    def test_vector_scalar(self):
+        v = [datetime(2018, 12, 15), datetime(2018, 12, 15)]
+        s = datetime(2019, 3, 1)
+        x = yearfrac(v, s, 'act')
+        self.assertEquals(
+            [round(x[0], 8), round(x[1], 8)],
+            [0.20547945, 0.20547945]
+        )
+
+    def test_scalar_vector(self):
+        s = datetime(2018, 12, 15)
+        v = [datetime(2019, 3, 1), datetime(2019, 3, 1)]
+        x = yearfrac(s, v, 'act')
+        self.assertEquals(
+            [round(x[0], 8), round(x[1], 8)],
+            [0.20547945, 0.20547945]
+        )
+
+    def test_nparray_vector(self):
+        v1 = np.array(
+            [datetime(2018, 12, 15), datetime(2018, 12, 15)]
+        )
+        v2 = [datetime(2019, 3, 1), datetime(2019, 3, 1)]
+        x = yearfrac(v1, v2, 'act')
+        self.assertEquals(
+            [round(x[0], 8), round(x[1], 8)],
+            [0.20547945, 0.20547945]
+        )
+
+    def test_pdseries_vector(self):
+        v1 = pd.Series(
+            [datetime(2018, 12, 15), datetime(2018, 12, 15)]
+        )
+        v2 = [datetime(2019, 3, 1), datetime(2019, 3, 1)]
+        x = yearfrac(v1, v2, 'act')
+        self.assertEquals(
+            [round(x[0], 8), round(x[1], 8)],
+            [0.20547945, 0.20547945]
+        )
 
 
 # run
--- yearfrac/yearfrac.py
+++ yearfrac/yearfrac.py
@@ -1,19 +1,59 @@
-
-import datetime
+import collections
 from .act_isda import act_isda
 from .act_afb import act_afb
 
 
-def yearfrac(d1, d2, method='act'):
-    if not isinstance(d1, (datetime.datetime, datetime.date)):
-        raise Exception("d1 is not a datetime object.")
-
-    if not isinstance(d2, (datetime.datetime, datetime.date)):
-        raise Exception("d2 is not a datetime object.")
-
+def __get_method_function(method):
     if method in ('act_afb', 'act'):
-        return act_afb(d1.year, d1.month, d1.day, d2.year, d2.month, d2.day)
-    elif method in ('act_isda'):
-        return act_isda(d1.year, d1.month, d1.day, d2.year, d2.month, d2.day)
-    else:
-        raise Exception("no method {0:s}".format(method))
+        return act_afb
+    elif method == 'act_isda':
+        return act_isda
+    raise Exception("no method {0:s}".format(method))
+
+
+def __yearfrac_scalar_scalar(s1, s2, method_function):
+    return method_function(
+        s1.year, s1.month, s1.day,
+        s2.year, s2.month, s2.day
+    )
+
+
+def __yearfrac_scalar_vector(s, v, method_function):
+    sv = [s]*len(v)
+    __yearfrac_vector_vector(sv, v, method_function)
+
+
+def __yearfrac_vector_scalar(v, s, method_function):
+    sv = [s]*len(v)
+    __yearfrac_vector_vector(v, sv, method_function)
+
+
+def __yearfrac_vector_vector(v1, v2, method_funciton):
+    rows = zip(v1, v2)
+    return [
+        method_funciton(
+            s1.year, s1.month, s1.day, s2.year, s2.month, s2.day
+        )
+        for s1, s2 in rows
+    ]
+
+
+__algorithm_mapper = {
+        (True, True): __yearfrac_vector_vector,
+        (False, False): __yearfrac_scalar_scalar,
+        (True, False):
+            lambda v, s, m: __yearfrac_vector_vector(v, [s]*len(v), m),
+        (False, True):
+            lambda s, v, m: __yearfrac_vector_vector([s]*len(v), v, m)
+    }
+
+
+def yearfrac(d1, d2, method='act'):
+    yearfrac_func = __algorithm_mapper[
+        (
+            isinstance(d1, collections.Iterable),
+            isinstance(d2, collections.Iterable)
+        )
+    ]
+    method_function = __get_method_function(method)
+    return yearfrac_func(d1, d2, method_function)
