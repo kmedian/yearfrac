dd8c43de35eccbf3570adf1e1a6430c0dc4e0424
UH <554c46@gmail.com>
2021-04-08 11:19:37 +0200

    rst for pypi, zip file install

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='yearfrac',
       version='0.4.4',
       description='Daycount methods to compute date differences in year units',
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/kmedian/yearfrac',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -20,4 +15,4 @@ setup(name='yearfrac',
           'setuptools>=40.0.0',
           'numpy>=1.14.*'],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
